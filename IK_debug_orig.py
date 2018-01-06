9fcb761e26be21f9b83022b9bf7b9569f8879e99
Ivo Georgiev <ivogeorg@gmail.com>
2018-01-05 17:46:32 -0700

    ik impl partial

--- IK_debug_orig.py
+++ IK_debug_orig.py
@@ -64,54 +64,14 @@ def test_code(test_case):
 
     ## Insert IK code here!
     
-    # theta1 = 0
-    # theta2 = 0
-    # theta3 = 0
-    # theta4 = 0
-    # theta5 = 0
-    # theta6 = 0
+    theta1 = 0
+    theta2 = 0
+    theta3 = 0
+    theta4 = 0
+    theta5 = 0
+    theta6 = 0
 
-    # Define DH param symbols
-    d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8') # link offsets
-    a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7') # link lengths
-    alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7') # twist angles
-
-    # Joint angle symbols
-    q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
-
-    # See KR210 Forward Kinematics
-    # Modified DH param
-    DH_Table = {
-        alpha0:       0, a0:      0, d1:  0.75, q1:           q1,
-        alpha1: -pi/2.0, a1:   0.35, d2:     0, q2: -pi/2.0 + q2,
-        alpha2:       0, a2:   1.25, d3:     0, q3:           q3,
-        alpha3: -pi/2.0, a3: -0.054, d4:   1.5, q4:           q4,
-        alpha4:  pi/2.0, a4:      0, d5:     0, q5:           q5,
-        alpha5: -pi/2.0, a5:      0, d6:     0, q6:           q6,
-        alpha6:       0, a6:      0, d7: 0.303, q7:            0
-    }
-
-    # Define modified DH transformation matrix
-    def TF_Matrix(alpha, a, d, q):
-        TF = Matrix([
-            [           cos(q),           -sin(q),           0,             a],
-            [sin(q)*cos(alpha), cos(q)*cos(alpha), -sin(alpha), -sin(alpha)*d],
-            [sin(q)*sin(alpha), cos(q)*sin(alpha),  cos(alpha),  cos(alpha)*d],
-            [                0,                 0,           0,             1]
-        ])
-
-    T0_1 =  TF_Matrix(alpha0, a0, d1, q1).subs(DH_Table)
-    T1_2 =  TF_Matrix(alpha1, a1, d2, q2).subs(DH_Table)
-    T2_3 =  TF_Matrix(alpha2, a2, d3, q3).subs(DH_Table)
-    T3_4 =  TF_Matrix(alpha3, a3, d4, q4).subs(DH_Table)
-    T4_5 =  TF_Matrix(alpha4, a4, d5, q5).subs(DH_Table)
-    T5_6 =  TF_Matrix(alpha5, a5, d6, q6).subs(DH_Table)
-    T6_EE = TF_Matrix(alpha6, a6, d7, q7).subs(DH_Table)
-
-    T0_EE = T0_1 + T1_2 + T2_3 + T3_4 + T4_5 + T5_6 + T6_EE
-
-
-    ##
+    ## 
     ########################################################################################
     
     ########################################################################################
